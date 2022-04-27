98a8cc4c15c9b09e3eafda05c3450c5f67aa6587
potluu <50609453+potluu@users.noreply.github.com>
2022-04-27 12:17:53 +0700

    Update projetc

--- src/main/java/com/ecommerce/project/laptop/utils/JwtUtils.java
+++
@@ -1,4 +0,0 @@
-package com.ecommerce.project.laptop.utils;
-
-public class JwtUtils {
-}
--- src/main/java/com/ecommerce/project/laptop/utils/StringUlis.java
+++ src/main/java/com/ecommerce/project/laptop/utils/StringUlis.java
@@ -3,14 +3,17 @@ package com.ecommerce.project.laptop.utils;
 import java.util.Random;
 
 public class StringUlis {
-    private static final String ASCII = "ABCDEFGHTMALKUGCRCTVGCG";
-    private static final int[] NUMBER = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    private Random random = new Random();
-
+    private static final String ASCII = "ABCDEFGHTMALKUGCRCTVGCG1234567890";
     public static String createSKU(String sku, int numberOfCharacter) {
+        Random random= new Random();
+StringBuffer buffer = new StringBuffer();
+        String str="";
+        char[] chars=ASCII.toCharArray();
         for (int i = 0; i < numberOfCharacter; i++) {
-
+            int r=random.nextInt(ASCII.length());
+            char c=ASCII.charAt(r);
+            buffer.append(c);
         }
-        return sku;
+        return sku+buffer.toString();
     }
 }
